1c15ffbc6b0a13addf757b543151836beca9f94b
OlgaHook <olga_krjuckova@inbox.lv>
2021-08-11 14:55:20 +0300

    Initial Commit

---
+++ AppleNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleNewsFeed
+//
+//  Created by olga.krjuckova on 11/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
